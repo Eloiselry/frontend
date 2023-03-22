5383d4eb9ac6cb8c30d9d08f8cd4de19f58162a5
27010-17-14 <27010-17-14@exchange.ad.afpanet>
2023-03-22 16:41:09 +0100

    nouveau dossier js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,84 @@
-console.log("coucou");
+
+
+
+
+/*var reponse1 = window.prompt("Saisissez votre nom");
+var reponse2 = window.prompt("Saisissez votre prenom");
+if (window.confirm("etes vous une femme ?") == true) 
+
+alert("Bonjour madame Leroy Eloïse bienvenue sur notre site web");*/
+
+
+/*var nombre = prompt ("entre un nombre");
+
+console.log(nombre);
+if(nombre%2==0)
+{
+    alert("pair");
+}
+
+else{
+    alert("impair");
+}*/
+
+/*var annee =prompt("entrez annee");
+console.log(annee);
+let YearNow = new Date();
+let YearNowOk = YearNow.getFullYear();
+    console.log(YearNow);
+    console.log(YearNowOk);
+var age = YearNowOk-annee;
+console.log(age);
+if(age=18)
+{
+    alert("majeur");
+}
+else{
+    alert("mineur");
+}*/
+
+
+/*let nombre1 = prompt("veuillez saisir un nombre"); 
+
+console.log(nombre1);
+let nombre2 = prompt("veuillez saisir un nombre");
+console.log(nombre2);
+var operateur = prompt ("veuillez saisir un operateur")
+console.log(operateur);
+if (isNaN(nombre1)==true || isNaN(nombre2)== true)
+{
+    alert("le nombre1 n'est pas correct");
+}
+
+function operation(nombre1,nombre2,operateur)
+{
+if(operateur=="+")
+{
+    return parseInt(nombre1) + parseInt(nombre2);
+}
+
+else if (operateur=="-")
+{
+    return nombre1-nombre2;
+}
+else if (operateur=="/")
+{
+   return nombre1/nombre2;
+}
+else if (operateur=="*")
+{
+    return nombre1*nombre2;
+}
+}
+
+if ( nombre2==0 && operateur=="/")
+{
+    alert("le nombre 0 n'est pas divisible")
+
+}
+else{
+    console.log(operation(nombre1,nombre2,operateur));
+}*/
+
+
+
